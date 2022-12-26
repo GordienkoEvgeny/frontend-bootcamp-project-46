c3e1dfc1f2e2bbfee948d55d85d07b335e69f100
EvgenyGordienko <evgeny.gordienko95@gmail.com>
2022-12-26 16:08:07 +0300

    add file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/GordienkoEvgeny/frontend-bootcamp-project-46/workflows/hexlet-check/badge.svg)](https://github.com/GordienkoEvgeny/frontend-bootcamp-project-46/actions)
+### Asciinema (step 3)
+https://asciinema.org/connect/43e9a345-320b-4eb0-ae6a-581e91c664ce
---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import _ from 'lodash';
+/* eslint-disable no-console */
+import fs from 'fs';
+import process from 'process';
+import path from 'path';
+
+const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), path.join('./src', filename)));
+
+const genDiff = (file1, file2) => {
+  const firstObject = readFile(file1);
+  const secondObject = readFile(file2);
+  const data1 = JSON.parse(firstObject);
+  const data2 = JSON.parse(secondObject);
+  const firstObjectKeys = Object.keys(data1);
+  const secondObjectKeys = Object.keys(data2);
+  const unionObjects = { ...data1, ...data2 };
+  const unionObjectsEntries = Object.entries(unionObjects).sort();
+  const result = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [key, value] of unionObjectsEntries) {
+    if (firstObjectKeys.includes(key) && secondObjectKeys.includes(key) && data1[key] === value) {
+      result[`  ${key}`] = data1[key];
+    } else if (!firstObjectKeys.includes(key)) {
+      result[`+ ${key}`] = data2[key];
+    } else if (firstObjectKeys.includes(key) && secondObjectKeys.includes(key)
+     && data1[key] !== value) {
+      result[`- ${key}`] = data1[key];
+      result[`+ ${key}`] = data2[key];
+    } else if (firstObjectKeys.includes(key) && !secondObjectKeys.includes(key)) {
+      result[`- ${key}`] = data1[key];
+    }
+  }
+  const toString = JSON.stringify(result, null, '   ');
+  return toString.replace(/["']/g, '');
+};
+export default genDiff;
