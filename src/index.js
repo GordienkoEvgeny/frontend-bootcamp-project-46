ece8f24595e936b1d2e7a0995a8b1e2bc4d1e926
EvgenyGordienko <evgeny.gordienko95@gmail.com>
2023-01-02 17:15:53 +0300

    fix file index.js, parser.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const getFormat = (filename) => path.extname(filename);
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const firstFileFormat = getFormat(filepath1);
   const secondFileFormat = getFormat(filepath2);
-  const firstObject = readFile(filepath1);
-  const secondObject = readFile(filepath2);
-  const data1 = parse(firstFileFormat, firstObject);
-  const data2 = parse(secondFileFormat, secondObject);
-  const innerTree = buildTree(data1, data2);
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+  const firstObj = parse(firstFileFormat, data1);
+  const secondObj = parse(secondFileFormat, data2);
+  const innerTree = buildTree(firstObj, secondObj);
   return checkFormat(innerTree, formatName);
 };
 export default genDiff;
