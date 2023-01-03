1be3230fe216e3320571fc99713f38f70081c96e
EvgenyGordienko <evgeny.gordienko95@gmail.com>
2023-01-03 09:47:45 +0300

    fix readFile

--- src/readFile.js
+++ src/readFile.js
@@ -5,6 +5,6 @@ import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+export const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 export default readFile;
